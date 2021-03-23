998956fd0c111d609948dc8a7f43f3f3c6177a1f
tanusreehui <tanusreehui@gmail.com>
2021-03-23 22:48:38 +0530

    sort

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
-print("test")
+x=list()
+print("Enter the value")
+for i in range(0,3):
+    a=int(input("Enter the number  "))
+    x.append(a)
+print(x)
+x.sort(reverse=True)
+print(x)
+ans=1
+p=1
+i=0
+while True:
+    if x[i]%p==0 && x[i+1]%p==0 :
+        print("test")
+    p=p+1
